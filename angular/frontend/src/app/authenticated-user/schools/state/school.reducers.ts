51f1b748578f29b8d39b03578274ee718088c878
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-08-08 14:53:20 +0800

    Schools ngrx reducer -- angular/frontend

---
+++ angular/frontend/src/app/authenticated-user/schools/state/school.reducers.ts
@@ -0,0 +1,166 @@
+import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
+
+import { SchoolActions, SchoolActionTypes } from './school.actions';
+import { SchoolModel } from 'src/app/models/school.model';
+
+
+
+function compareSchoolsByName(
+    a:SchoolModel, b:SchoolModel) {
+    const schoolA = a.school_name;
+    const schoolB = b.school_name;
+  
+    let comparison = 0;
+    if (schoolA > schoolB) {
+      comparison = 1;
+    } else if (schoolA < schoolB) {
+      comparison = -1;
+    }
+    return comparison;
+};
+
+export interface SchoolsState extends EntityState<SchoolModel> {
+    fetchingSchoolsInProgress: boolean;
+    errorMessage: string | undefined,
+    schoolsLoaded: boolean,
+    successMessage: string | undefined,
+};
+
+export const adapter: EntityAdapter<SchoolModel> = 
+    createEntityAdapter<SchoolModel>(
+        { sortComparer: compareSchoolsByName }
+    );
+
+export const initialSchoolsState: SchoolsState = adapter.getInitialState({
+    errorMessage: undefined,
+    fetchingSchoolsInProgress: false,
+    schoolsLoaded: false,
+    successMessage: undefined
+});
+
+export function singleTasksReducer(
+    state = initialSchoolsState,
+    action: SchoolActions): SchoolsState {
+
+    let reducerErrorMessage: string | undefined = undefined;
+    let reducerSuccessMessage: string | undefined = undefined;
+
+    switch(action.type) {
+
+        case SchoolActionTypes.SchoolCreatedAdded:
+            reducerErrorMessage = undefined;
+            reducerSuccessMessage = 'Task successfully submitted!';
+            return adapter.addOne(action.payload.school, {...state,
+                errorMessage: reducerErrorMessage,
+                successMessage: reducerSuccessMessage
+            });
+
+        case SchoolActionTypes.SchoolCreationCancelled:
+            reducerErrorMessage = "Error submitting weekly task scheduler!";
+            reducerSuccessMessage = undefined;
+            if (action.payload.err.error.message) {
+                reducerErrorMessage = action.payload.err.error.message;
+            }
+            return {
+                ...state,  successMessage: reducerSuccessMessage,
+                errorMessage: reducerErrorMessage
+            }
+
+        case SchoolActionTypes.SchoolDeletionCancelled:
+            reducerErrorMessage = "Error! School Deletion Failed!";
+            reducerSuccessMessage = undefined;
+            if (action.payload.err.error.Error) {
+                reducerErrorMessage = action.payload.err.error.Error;
+            }
+            return {
+                ...state,  successMessage: reducerSuccessMessage,
+                errorMessage: reducerErrorMessage
+            }
+        
+        case SchoolActionTypes.SchoolDeletionSaved:
+            reducerErrorMessage = undefined;
+            reducerSuccessMessage = action.payload.message;
+            return adapter.removeOne(action.payload.id, 
+                { 
+                    ...state,
+                    errorMessage: undefined,
+                    successMessage: reducerSuccessMessage
+                }
+            );
+
+
+        case SchoolActionTypes.SchoolEditCancelled:
+            reducerErrorMessage = "Error! Editing School Failed!";
+            reducerSuccessMessage = undefined;
+
+            if (action.payload.err.error.Error) {
+                reducerErrorMessage = action.payload.err.error.Error;
+            }
+            return {
+                ...state,  successMessage: reducerSuccessMessage,
+                errorMessage: reducerErrorMessage
+            }
+        
+        case SchoolActionTypes.SchoolEditUpdated:
+            reducerErrorMessage = undefined;
+            reducerSuccessMessage = 'School information edtied!';
+            return adapter.updateOne(action.payload.school, 
+                {
+                    ...state,
+                    errorMessage: reducerErrorMessage,
+                    successMessage: reducerSuccessMessage
+                }
+            );
+   
+        case SchoolActionTypes.SchoolsMessagesCleared:
+            reducerErrorMessage = undefined;
+            reducerSuccessMessage = undefined;
+            return {
+                ...state,  successMessage: reducerSuccessMessage,
+                errorMessage: reducerErrorMessage
+            }
+
+        case SchoolActionTypes.SchoolsCleared:
+            return initialSchoolsState;
+    
+        case SchoolActionTypes.SchoolsLoaded:
+            reducerErrorMessage = undefined;
+            reducerSuccessMessage = undefined;
+    
+            return adapter.upsertMany(action.payload.schools, {...state,
+                errorMessage: reducerErrorMessage,
+                fetchingSchoolsInProgress: false,
+                schoolsLoaded: false,
+                successMessage: reducerSuccessMessage
+            });   
+
+        case SchoolActionTypes.SchoolsRequestCancelled:
+            reducerErrorMessage = "Error fetching schools!";
+            reducerSuccessMessage = undefined;
+
+            if (action.payload.err.error.message) {
+                reducerErrorMessage = action.payload.err.error.message;
+            }
+            return {
+                 ...state,  errorMessage: reducerErrorMessage, 
+                 successMessage: reducerSuccessMessage,
+            }
+
+        case SchoolActionTypes.SchoolsRequested:
+            return {
+                ...state, 
+                fetchingSchoolsInProgress: true
+            } 
+
+        default: {
+            return state
+        }
+    }
+
+}
+
+export const {
+    selectAll,
+    selectEntities,
+    selectIds,
+  } = adapter.getSelectors();
--- angular/frontend/src/app/models/school.model.ts
+++ angular/frontend/src/app/models/school.model.ts
@@ -1,4 +1,12 @@
 
+export interface SchoolCreateAndEditModel {
+  school_name: string;
+  address_line_1: string;
+  address_line_2: string;
+  contact_phone: string;
+  other_information: string;
+}
+
 export interface SchoolModel {
     id: number;
     school_name: string;
