83fa0ebccc4c5b69a0f0c35c40078207bfba8430
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-09-09 16:15:52 +0800

    Business logic to edit school info in school detail and school edit form components (html not written yet) -- angular/frontend

--- angular/frontend/src/app/authenticated-user/schools/detail/school-detail/school-detail.component.ts
+++ angular/frontend/src/app/authenticated-user/schools/detail/school-detail/school-detail.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import {select, Store } from '@ngrx/store';
+import { Observable, of } from "rxjs";
+
+import { SchoolModel } from 'src/app/models/school.model';
+import { SchoolsState } from '../../state/school.reducers';
+import { 
+  schoolsErrorMsg, schoolsSuccessMsg, 
+  selectSchoolById
+} from '../../state/school.selectors';
+import { SchoolsMessagesCleared } from '../../state/school.actions';
 
 @Component({
   selector: 'app-school-detail',
@@ -6,6 +17,34 @@ import { Component } from '@angular/core';
   templateUrl: './school-detail.component.html',
   styleUrl: './school-detail.component.css'
 })
-export class SchoolDetailComponent {
+export class SchoolDetailComponent implements OnInit {
+
+  errMsg$: Observable<string | undefined> = of(undefined);
+  idFromRouteData:number;
+  school$: Observable<SchoolModel | undefined>;
+  successMsg$: Observable<string | undefined> = of(undefined);
+
+  constructor(
+    private route: ActivatedRoute, 
+    private store: Store<SchoolsState>
+  ) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(new SchoolsMessagesCleared());
+    this.idFromRouteData = this.route.snapshot.params['id'];
+    this.school$ = this.store.pipe(select(
+      selectSchoolById(this.idFromRouteData)
+    ));
+    this.errMsg$ = this.store.pipe(
+      select(schoolsErrorMsg)
+    );
+    this.successMsg$ = this.store.pipe(
+      select(schoolsSuccessMsg)
+    );
+  }
+
+  onClearStatusMsgs() {
+    this.store.dispatch(new SchoolsMessagesCleared());
+  }
 
 }
--- angular/frontend/src/app/authenticated-user/schools/detail/school-edit-form/school-edit-form.component.ts
+++ angular/frontend/src/app/authenticated-user/schools/detail/school-edit-form/school-edit-form.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Store} from '@ngrx/store';
+
+import { 
+  SchoolCreateAndEditModel, SchoolModel 
+} from 'src/app/models/school.model';
+import { SchoolsState } from '../../state/school.reducers';
+import { 
+  SchoolEditCancelled, SchoolEditSubmitted 
+} from '../../state/school.actions';
 
 @Component({
   selector: 'app-school-edit-form',
@@ -7,5 +17,34 @@ import { Component } from '@angular/core';
   styleUrl: './school-edit-form.component.css'
 })
 export class SchoolEditFormComponent {
+
+  @Input() school: SchoolModel;
+
+  constructor(private store: Store<SchoolsState>) { }
+
+
+  onSubmitEditedSchool(form: NgForm) {
+    console.log(form.value)
+    if (form.invalid) {
+      this.store.dispatch(new SchoolEditCancelled({err: {
+        error: {
+          Error: "The form value was not properly filled in!"
+        }
+      }} ));
+      form.reset();
+      return;
+    }
+    let editedSchoolData: SchoolCreateAndEditModel = {
+      school_name: form.value.school_name,
+      address_line_1: form.value.address_line_1,
+      address_line_2: form.value.address_line_2,
+      contact_phone: form.value.contact_phone,
+      other_information: form.value.other_information,    }
+    this.store.dispatch(new SchoolEditSubmitted(
+      {  id: this.school.id, school: editedSchoolData }
+    ));
+    form.resetForm();
+  }
+
 
 }
