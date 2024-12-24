8199207d6d93f4ea462dbf0238dd074c2d6143d5
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-12-24 14:35:03 +0800

    ngrx selectors to implement fetching of landing page schedule and daily tasks schedule -- angular/frontend

---
+++ angular/frontend/src/app/authenticated-user/scheduling/classes-state/scheduled-classes.selectors.ts
@@ -0,0 +1,36 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+
+import { ScheduledClassesState } from "./scheduled-classes.reducers";
+import * as fromScheduledClasses from "./scheduled-classes.reducers";
+
+export const selectScheduledClassesState = 
+            createFeatureSelector<ScheduledClassesState>("scheduledClasses");
+
+export const selectAllScheduledClasses = createSelector(
+    selectScheduledClassesState,
+    fromScheduledClasses.selectAll
+);
+
+export const selectScheduledClassById = (id:number) => createSelector(
+    selectScheduledClassesState,
+    scheduledClassesState => scheduledClassesState.entities[id]
+);
+  
+export const selectScheduledClassesByDate = (date: string) => createSelector(
+    selectAllScheduledClasses,
+    scheduledClassesState => {
+        return scheduledClassesState.filter(
+            scheduledClass => scheduledClass.date == date
+        );
+    }
+);
+
+export const fetchingClassesInProgress = createSelector(
+    selectScheduledClassesState,
+    scheduledClassesState => scheduledClassesState.fetchingClassesInProgress
+);
+              
+export const landingPageTasksLoaded = createSelector(
+    selectScheduledClassesState,
+    scheduledClassesState => scheduledClassesState.landingPageScheduledClassesLoaded
+);
