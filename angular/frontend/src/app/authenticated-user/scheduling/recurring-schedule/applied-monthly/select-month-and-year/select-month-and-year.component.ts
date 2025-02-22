a1639507845c91b54ccf6fbea46326f1867a4416
phil-in-taipei <sweeneyphilip11@gmail.com>
2025-02-22 14:29:11 +0800

    Setup to select month and year in component to navigate to recurring classes applied monthly component, which will list all recurring classes applied to month/year and optionally display form for user to apply recurring classes to the given month/year -- angular/frontend

--- angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/applied-monthly/select-month-and-year/select-month-and-year.component.ts
+++ angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/applied-monthly/select-month-and-year/select-month-and-year.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { getYearsOptions, monthsAndIntegers } from 'src/app/shared-utils/date-time.util';
 
 @Component({
   selector: 'app-select-month-and-year',
@@ -6,6 +10,31 @@ import { Component } from '@angular/core';
   templateUrl: './select-month-and-year.component.html',
   styleUrl: './select-month-and-year.component.css'
 })
-export class SelectMonthAndYearComponent {
+export class SelectMonthAndYearComponent implements OnInit {
+
+  years: number[];
+
+  readonly monthsAndIntegers = monthsAndIntegers;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.years = getYearsOptions();
+  }
+
+
+  onMonthAndYearNav(form: NgForm) {
+    console.log(form.value);
+    if (form.invalid) {
+      return;
+    }
+    const month = form.value.month;
+    const year = form.value.year;
+    this.router.navigate(
+      ['/', 'authenticated-user', 'scheduling', 'recurring-schedule', 'applied-monthly', month, year],
+    );
+
+  }
+
 
 }
--- angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/recurring-schedule-routing.module.ts
+++ angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/recurring-schedule-routing.module.ts
@@ -3,14 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { 
   CreateRecurringClassComponent 
 } from './create/create-recurring-class/create-recurring-class.component';
+import { 
+  RecurringClassesAppliedMonthlyComponent 
+} from './applied-monthly/recurring-classes-applied-monthly/recurring-classes-applied-monthly.component';
 import { RecurringClassesComponent } from './list/recurring-classes/recurring-classes.component';
 import { RecurringScheduleComponent } from './recurring-schedule/recurring-schedule.component';
+import { 
+  SelectMonthAndYearComponent 
+} from './applied-monthly/select-month-and-year/select-month-and-year.component';
 
 const routes: Routes = [
   { path: '', component: RecurringScheduleComponent, 
     children: [
       { path: 'create', component: CreateRecurringClassComponent },
+      { 
+        path: 'applied-monthly/:month/:year', 
+        component: RecurringClassesAppliedMonthlyComponent 
+      },
       { path: 'list', component: RecurringClassesComponent },
+      { path: 'select-month-year', component: SelectMonthAndYearComponent },
     ] 
   }
 ];
