5c8cdd09f33c84d3716c6d02251389ace6dadf98
phil-in-taipei <sweeneyphilip11@gmail.com>
2025-03-01 17:16:43 +0800

    Typescript business logic in apply-recurring-class-monthly-formto prepare for user to be able to toggle form in the monthly list view of recurring classes applied by monthly and year and apply more recurring classes to the given time period -- angular/frontend

--- angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/applied-monthly/apply-recurring-class-monthly-form/apply-recurring-class-monthly-form.component.ts
+++ angular/frontend/src/app/authenticated-user/scheduling/recurring-schedule/applied-monthly/apply-recurring-class-monthly-form/apply-recurring-class-monthly-form.component.ts
@@ -1,4 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+
+import { 
+  RecurringClassAppliedMonthlyCreateModel, 
+  RecurringClassModel
+} from 'src/app/models/recurring-schedule.model';
+import { 
+  RecurringClassesState 
+} from '../../state/recurring-schedule-state/recurring-schedule.reducers';
+import { 
+  recurringClassAppliedMonthlysErrorMsg, recurringClassAppliedMonthysSuccessMsg 
+} from '../../state/recurring-classes-applied-monthly-state/recurring-class-applied-monthly.selectors';
+import { 
+  RecurringClassAppliedMonthlysState 
+} from '../../state/recurring-classes-applied-monthly-state/recurring-class-applied-monthly.reducers';
+import { 
+  RecurringClassAppliedMonthlyCreationCancelled,
+  RecurringClassAppliedMonthlyCreateSubmitted,
+  RecurringClassesAppliedMonthlyMessagesCleared 
+} from '../../state/recurring-classes-applied-monthly-state/recurring-class-applied-monthly.actions';
+import { 
+  selectAllRecurringClasses 
+} from '../../state/recurring-schedule-state/recurring-schedule.selectors';
 
 @Component({
   selector: 'app-apply-recurring-class-monthly-form',
@@ -6,6 +31,61 @@ import { Component } from '@angular/core';
   templateUrl: './apply-recurring-class-monthly-form.component.html',
   styleUrl: './apply-recurring-class-monthly-form.component.css'
 })
-export class ApplyRecurringClassMonthlyFormComponent {
+export class ApplyRecurringClassMonthlyFormComponent implements OnInit {
+
+
+  recurringClasses$: Observable<RecurringClassModel[] | undefined> = of(undefined);
+  @Input() month:number;
+  @Input() year:number;
+  errMsg$: Observable<string | undefined>;
+  successMsg$: Observable<string | undefined>;
+
+
+  constructor( 
+    private rCAMStore: Store<RecurringClassAppliedMonthlysState>,
+    private recurringClassesStore: Store<RecurringClassesState>
+  ) {}
+
+
+  ngOnInit(): void {
+    this.rCAMStore.dispatch(
+      new RecurringClassesAppliedMonthlyMessagesCleared()
+    );
+    this.errMsg$ = this.rCAMStore.pipe(
+      select(recurringClassAppliedMonthlysErrorMsg)
+    );
+    this.successMsg$ = this.rCAMStore.pipe(
+      select(recurringClassAppliedMonthysSuccessMsg)
+    );
+    this.recurringClasses$ = this.recurringClassesStore.pipe(
+      select(selectAllRecurringClasses)
+    );
+  }
+
+  onClearStatusMsgs() {
+    this.rCAMStore.dispatch(new RecurringClassesAppliedMonthlyMessagesCleared());
+  }
+
+  onSubmitRCAM(form: NgForm) {
+
+    if (form.invalid) {
+      this.rCAMStore.dispatch(new RecurringClassAppliedMonthlyCreationCancelled({err: {
+        error: {
+          message: "The form values were not properly filled in!"
+        }
+      }} ));
+      form.reset();
+    }
+    let submissionForm: RecurringClassAppliedMonthlyCreateModel = {
+        recurring_class: form.value.monthlyScheduler,
+        scheduling_month: this.month,
+        scheduling_year: this.year
+    }
+    console.log(submissionForm);
+    this.rCAMStore.dispatch(new RecurringClassAppliedMonthlyCreateSubmitted(
+      { recurringClassAppliedMonthly: submissionForm }
+    ));
+    form.reset();
+    }
 
 }
