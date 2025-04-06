5a385e01a0d2582ef26b1910ea798665867b462f
phil-in-taipei <sweeneyphilip11@gmail.com>
2025-04-06 15:03:13 +0800

    Component for school report within date range's business logic and beginning of template to display report, error message, or spinner when api call is triggered upon initialization using variables from the route url -- angular/frontend

--- angular/frontend/src/app/authenticated-user/scheduling/accounting/reports/school-accounting-report-within-date-range/school-accounting-report-within-date-range.component.ts
+++ angular/frontend/src/app/authenticated-user/scheduling/accounting/reports/school-accounting-report-within-date-range/school-accounting-report-within-date-range.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { single } from 'rxjs';
+import { ActivatedRoute } from "@angular/router";
+import { SchoolAccountingReportModel } from 'src/app/models/accounting.model';
+
+import { AccountingService } from '../../accounting-service/accounting.service';
+
 
 @Component({
   selector: 'app-school-accounting-report-within-date-range',
@@ -7,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrl: './school-accounting-report-within-date-range.component.css'
 })
 export class SchoolAccountingReportWithinDateRangeComponent {
+
+
+  errorMessage: string|undefined = undefined;
+  fetchingReportInProgress:boolean = true;
+  startDateFromRouteData:string;
+  finishDateFromRouteData:string;
+  schoolIdFromRouteData:number;
+  schoolAccountingReport: SchoolAccountingReportModel|undefined = undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private accountingService: AccountingService
+  ) { }
+
+  onClearErrorMessage() {
+    this.errorMessage = undefined;
+  }
+
+  ngOnInit(): void {
+    this.startDateFromRouteData = this.route.snapshot.params['start_date'];
+    this.finishDateFromRouteData = this.route.snapshot.params['finish_date'];
+    this.schoolIdFromRouteData = +this.route.snapshot.params['school_id'];
+    this.accountingService.fetchSchoolAccountingReportWithinDateRange(
+      this.startDateFromRouteData,
+      this.finishDateFromRouteData,
+      this.schoolIdFromRouteData
+    ).pipe(single()
+      ).subscribe({
+        next: (res) => { 
+          this.schoolAccountingReport = res; 
+          this.fetchingReportInProgress = false;
+        },
+        error: (err) => {
+          this.errorMessage = 'There was an error fetching the report';
+          this.fetchingReportInProgress = false;
+          if (err["detail"]) {
+            this.errorMessage = err["detail"];
+          }
+        }
+      }
+    );
+  }
 
 }
