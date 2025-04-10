9d8863a5111223ad628aa46e1f6c82cf5abba4bf
phil-in-taipei <sweeney.philip11@gmail.com>
2025-04-10 15:32:12 +0800

    Forgot to save before code in purchasing form before last commit. Remaining unsaved code -- angular/frontend

--- angular/frontend/src/app/authenticated-user/scheduling/accounting/freelance-transactions/purchases/make-purchase-form/make-purchase-form.component.ts
+++ angular/frontend/src/app/authenticated-user/scheduling/accounting/freelance-transactions/purchases/make-purchase-form/make-purchase-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { single } from 'rxjs';
 
 import { AccountingService } from '../../../accounting-service/accounting.service';
 import { 
@@ -18,9 +19,41 @@ export class MakePurchaseFormComponent {
 
   errorMessage: string | undefined = undefined;
   @Input() freelanceStudents: StudentOrClassModel[];
+  fetchingInProgress: boolean = false;
   freelanceTuitionTransactionResponse: FreelanceTuitionTransactionRecordModel | undefined = undefined;
+  showForm: boolean = true;
   sucessMessage: string | undefined = undefined;
 
   constructor(private accountingService: AccountingService) { }
 
+  onSubmitPurchase(form: NgForm) {
+    if (form.invalid) {
+      form.reset();
+      return;
+    }
+    let submissionForm: FreelanceTuitionTransactionModel = {
+      transaction_type: "purchase", 
+      class_hours_purchased_or_refunded: +form.value.class_hours_purchased_or_refunded,
+      student_or_class: +form.value.student_or_class
+    }
+    this.accountingService.submitFreelanceTuitionTransaction(
+      submissionForm
+    ).pipe(single()
+      ).subscribe({
+        next: (res) => { 
+          this.freelanceTuitionTransactionResponse = res; 
+          this.fetchingInProgress = false;
+          this.showForm = false;
+        },
+        error: (err) => {
+          this.errorMessage = 'There was an error submitting the transactions';
+          this.fetchingInProgress = false;
+          if (err["message"]) {
+            this.errorMessage = err["message"];
+          }
+          this.showForm = true;
+        }
+      }
+    );
+  }
 }
