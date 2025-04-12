c308384003299adf0bc264926410a155a53313d1
phil-in-taipei <sweeneyphilip11@gmail.com>
2025-04-12 14:03:59 +0800

    Updated environment variables in Anglular to more accurately reflect the timer cycle for fetching tokens and refresh tokens during deployment. During most of development the cycle has been very short because of itial debugging and testing -- angular/frontend

--- angular/frontend/src/environments/environment.ts
+++ angular/frontend/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:8000",
-  authTimerAmount: 50000, //(50 seconds)
-  tokenRefreshHoursAmount: 0,
-  tokenRefreshMinsAmount: 3,
-  tokenRefreshSecondsAmount: 30,
-  tokenMinsAmount: 0,
-  tokenSecondsAmount: 50,
+  authTimerAmount: 590000, //(9 minutes and 50 seconds)
+  tokenRefreshHoursAmount: 5,
+  tokenRefreshMinsAmount: 59,
+  tokenRefreshSecondsAmount: 51,
+  tokenMinsAmount: 9,
+  tokenSecondsAmount: 51,
 };
 
 /*
