59ddb8d4c9284c22bc54e1be5a1f0266599e7f47
phil-in-taipei <sweeneyphilip11@gmail.com>
2025-06-14 13:54:56 +0800

    Fixed error with success message not displaying in registration component and lowered the minimum number of characters for given name and surname fields to 2 characters -- angular/frontend

--- angular/frontend/src/app/registration/registration.service.ts
+++ angular/frontend/src/app/registration/registration.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { environment } from '../../environments/environment';
 import { UserRegistrationModel, 
@@ -15,8 +14,8 @@ export class RegistrationService {
   constructor(private http: HttpClient) { }
 
   submitUserRegistration(userRegistration: UserRegistrationModel) {
-    console.log("This is the payload in the service:")
-    console.log(userRegistration);
+    //console.log("This is the payload in the service:")
+    //console.log(userRegistration);
       return this.http.post<UserRegistrationResponseModel>(
         `${environment.apiUrl}/api/profiles/user/`, userRegistration)
   }
