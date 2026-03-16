e7e9eacf4d9c350faf6ab64e9c3280c86c166eab
phil-in-taipei <sweeneyphilip11@gmail.com>
2026-03-16 15:39:35 +0800

    Updated bundles and modification of user profile model to distinguish different user types -- django/backend

--- django/backend/user_profiles/models.py
+++ django/backend/user_profiles/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.conf import settings
 
 
+ACCOUNT_TYPE = (
+    ('teacher', 'Teacher'),
+    ('venue_owner', 'Venue_Owner'),
+)
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # note: next time eliminate the null=True and blank=True in the fields below
@@ -9,6 +15,7 @@ class UserProfile(models.Model):
     contact_email = models.EmailField(max_length=200, null=True, blank=True)
     surname = models.CharField(max_length=120, null=True, blank=True)
     given_name = models.CharField(max_length=120, null=True, blank=True)
+    account_type = models.CharField(max_length=200, choices=ACCOUNT_TYPE, default='teacher')
 
     class Meta:
         ordering = ('surname', 'given_name')
