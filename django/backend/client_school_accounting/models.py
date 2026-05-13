c625cf8b176d75b53a1bbf5577f37494c70aec7b
phil-in-taipei <sweeney.philip11@gmail.com>
2026-05-13 16:30:46 +0800

    Revision to client school student account model to add a field for purchased company classes and date field for expiration times for each of the types of purchased hours. In the Meta class constraints were also added to ensure that if there are no purchased hours for a type of class, then the corresponding expiration date field value is null. Because I had already added one object and forgot to delete it before making the changes, there were issues with the migration which required that I go in and manually delete fields in the mysql database using raw SQL commands. In the bridge class between the client school student account, and the student or class model for teachers, I also added in a foreign key field to connect to the account if the class is a company class. This entailed also adding an option to the class enrollement types tuple and updating the clean method in the model as well as the null checks in the constraints -- django/backend

--- django/backend/client_school_accounting/models.py
+++ django/backend/client_school_accounting/models.py
@@ -19,6 +19,7 @@ CLASS_ENROLLMENT_TYPES = (
     ('two_to_one_tutoring', 'Two-to-one Tutoring'),
     ('online_tutoring', 'Online Tutoring'),
     ('group_class', 'Group Class'),
+    ('company_class', 'Company Class'),
 )
 
 class StudentAccountBillingManager(models.Manager):
@@ -28,7 +29,8 @@ class StudentAccountBillingManager(models.Manager):
             if (
                 (account.purchased_group_class_hours is not None and account.purchased_group_class_hours <= 2) or
                 (account.purchased_tutoring_hours is not None and account.purchased_tutoring_hours <= 2) or
-                (account.purchased_online_hours is not None and account.purchased_online_hours <= 2)
+                (account.purchased_online_hours is not None and account.purchased_online_hours <= 2) or
+                (account.purchased_company_hours is not None and account.purchased_company_hours <= 2)
             )
         ]
 
@@ -52,11 +54,26 @@ class AccountingClientSchoolStudentAccount(models.Model):
     purchased_group_class_hours = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, null=True
     )
+    group_hours_expiration_date = models.DateField(
+        blank=True, null=True
+    )
     purchased_tutoring_hours = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, null=True
     )
+    tutoring_hours_expiration_date = models.DateField(
+        blank=True, null=True
+    )
     purchased_online_hours = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, null=True
+    )
+    online_hours_expiration_date = models.DateField(
+        blank=True, null=True
+    )
+    purchased_company_hours = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, null=True
+    )
+    company_hours_expiration_date = models.DateField(
+        blank=True, null=True
     )
 
     def __str__(self):
@@ -68,7 +85,66 @@ class AccountingClientSchoolStudentAccount(models.Model):
     class Meta:
         verbose_name_plural = 'Client School Student Accounts'
         unique_together = ('client_school', 'client_student_name')
-        ordering = ('client_school__school_name', 'client_student_name' )
+        ordering = ('client_school__school_name', 'client_student_name')
+        constraints = [
+            CheckConstraint(
+                check=(
+                    (
+                        Q(purchased_group_class_hours__isnull=True) &
+                        Q(group_hours_expiration_date__isnull=True)
+                    )
+                    |
+                    (
+                        Q(purchased_group_class_hours__isnull=False) &
+                        Q(group_hours_expiration_date__isnull=False)
+                    )
+
+                ),
+                name='group_hours_expiration_date_null_check'
+            ),
+            CheckConstraint(
+                check=(
+                    (
+                        Q(purchased_tutoring_hours__isnull=True) &
+                        Q(tutoring_hours_expiration_date__isnull=True)
+                    )
+                    |
+                    (
+                        Q(purchased_tutoring_hours__isnull=False) &
+                        Q(tutoring_hours_expiration_date__isnull=False)
+                    )
+                ),
+                name='tutoring_hours_expiration_date_null_check'
+            ),
+            CheckConstraint(
+                check=(
+                    (
+                        Q(purchased_online_hours__isnull=True) &
+                        Q(online_hours_expiration_date__isnull=True)
+                    )
+                    |
+                    (
+                        Q(purchased_online_hours__isnull=False) &
+                        Q(online_hours_expiration_date__isnull=False)
+                    )
+                ),
+                name='online_hours_expiration_date_null_check'
+            ),
+            CheckConstraint(
+                check=(
+                    (
+                        Q(purchased_company_hours__isnull=True) &
+                        Q(company_hours_expiration_date__isnull=True)
+                    )
+                    |
+                    (
+                        Q(purchased_company_hours__isnull=False) &
+                        Q(company_hours_expiration_date__isnull=False)
+                    )
+                ),
+                name='company_hours_expiration_date_null_check'
+            ),
+        ]
 
 
 
@@ -99,6 +175,12 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
         related_name='online_enrollment',
         blank=True, null=True
     )
+    client_school_company_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount,
+        on_delete=models.CASCADE,
+        related_name='company_enrollment',
+        blank=True, null=True
+    )
     client_group_accounts = models.ManyToManyField(
         AccountingClientSchoolStudentAccount,
         related_name='group_class_enrollments',
@@ -121,6 +203,10 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
                 raise ValidationError(
                     'One-to-one tutoring cannot have an online account.'
                 )
+            if self.client_school_company_account is not None:
+                raise ValidationError(
+                    'One-to-one tutoring cannot have a company account.'
+                )
 
         elif self.class_enrollment_type == 'two_to_one_tutoring':
             if self.client_school_one_to_one_account is not None:
@@ -130,6 +216,10 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
             if self.client_school_online_account is not None:
                 raise ValidationError(
                     'Two-to-one tutoring cannot have an online account.'
+                )
+            if self.client_school_company_account is not None:
+                raise ValidationError(
+                    'Two-to-one tutoring cannot have a company account.'
                 )
 
         elif self.class_enrollment_type == 'online_tutoring':
@@ -141,6 +231,10 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
                 raise ValidationError(
                     'Online tutoring cannot have a one-to-one account.'
                 )
+            if self.client_school_company_account is not None:
+                raise ValidationError(
+                    'Online tutoring cannot have a company account.'
+                )
 
         elif self.class_enrollment_type == 'group_class':
             if self.client_school_one_to_one_account is not None:
@@ -150,6 +244,24 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
             if self.client_school_online_account is not None:
                 raise ValidationError(
                     'Group class cannot have an online account.'
+                )
+            if self.client_school_company_account is not None:
+                raise ValidationError(
+                    'Group class cannot have a company account.'
+                )
+
+        elif self.class_enrollment_type == 'company_class':
+            if self.client_school_company_account is None:
+                raise ValidationError(
+                    'Company class requires a client_school_company_account.'
+                )
+            if self.client_school_one_to_one_account is not None:
+                raise ValidationError(
+                    'Company class cannot have a one-to-one account.'
+                )
+            if self.client_school_online_account is not None:
+                raise ValidationError(
+                    'Company class cannot have an online account.'
                 )
 
         if self.pk:
@@ -170,7 +282,9 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
                         'Group class cannot have two-to-one tutoring accounts.'
                     )
 
-            elif self.class_enrollment_type in ('one_to_one_tutoring', 'online_tutoring'):
+            elif self.class_enrollment_type in (
+                'one_to_one_tutoring', 'online_tutoring', 'company_class'
+            ):
                 if self.client_school_two_to_one_accounts.exists():
                     raise ValidationError(
                         '{} cannot have two-to-one tutoring accounts.'.format(
@@ -194,22 +308,33 @@ class ClientSchoolClassEnrollmentHandler(models.Model):
                         Q(class_enrollment_type='one_to_one_tutoring')
                         & Q(client_school_one_to_one_account__isnull=False)
                         & Q(client_school_online_account__isnull=True)
+                        & Q(client_school_company_account__isnull=True)
                     )
                     |
                     (
                         Q(class_enrollment_type='two_to_one_tutoring')
                         & Q(client_school_one_to_one_account__isnull=True)
                         & Q(client_school_online_account__isnull=True)
+                        & Q(client_school_company_account__isnull=True)
                     )
                     |
                     (
                         Q(class_enrollment_type='online_tutoring')
                         & Q(client_school_online_account__isnull=False)
                         & Q(client_school_one_to_one_account__isnull=True)
+                        & Q(client_school_company_account__isnull=True)
                     )
                     |
                     (
                         Q(class_enrollment_type='group_class')
+                        & Q(client_school_one_to_one_account__isnull=True)
+                        & Q(client_school_online_account__isnull=True)
+                        & Q(client_school_company_account__isnull=True)
+                    )
+                    |
+                    (
+                        Q(class_enrollment_type='company_class')
+                        & Q(client_school_company_account__isnull=False)
                         & Q(client_school_one_to_one_account__isnull=True)
                         & Q(client_school_online_account__isnull=True)
                     )
