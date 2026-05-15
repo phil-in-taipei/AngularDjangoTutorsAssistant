bdd8b99c501a0287a1c2df12b4ad39c80f56cde7
phil-in-taipei <sweeney.philip11@gmail.com>
2026-05-15 14:13:23 +0800

    Model for two-to-one tutoring tuition transactions for client school accounting services -- django/backend

--- django/backend/client_school_transactions/admin.py
+++ django/backend/client_school_transactions/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import ClientSchoolTutoringTuitionTransactionRecord
+from .models import (
+    ClientSchoolTutoringTuitionTransactionRecord, 
+    ClientSchool2to1TutoringTuitionTransactionRecord
+)
 
 admin.site.register(ClientSchoolTutoringTuitionTransactionRecord)
+admin.site.register(ClientSchool2to1TutoringTuitionTransactionRecord)
+
--- django/backend/client_school_transactions/models.py
+++ django/backend/client_school_transactions/models.py
@@ -77,3 +77,61 @@ class ClientSchoolTutoringTuitionTransactionRecord(models.Model):
     class Meta:
         verbose_name_plural = 'Tutoring Tuition Transaction Records'
         ordering = ('-time_stamp',)
+
+
+class ClientSchool2to1TutoringTuitionTransactionRecord(models.Model):
+    primary_student_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
+        related_name='two_to_one_tutoring_tuition_transactions',
+        limit_choices_to={'purchased_tutoring_hours__isnull': False}
+    )
+    shared_student_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
+        related_name='two_to_one_shared_tutoring_tuition_transactions',
+        limit_choices_to={'purchased_tutoring_hours__isnull': False}
+    )
+    transaction_amount = models.PositiveSmallIntegerField(
+        validators=[validate_tuition_transaction_amount], default=16500
+    )
+    transaction_type = models.CharField(
+        max_length=200, choices=TRANSACTION_TYPE, default='payment'
+    )
+    class_hours_purchased_or_refunded = models.PositiveSmallIntegerField(
+        validators=[validate_number_of_hours_purchased]
+    )
+    expiration_period = models.CharField(
+        max_length=200, choices=TUTORING_EXPIRATION_PERIODS, default='6_months'
+    )
+    comments = models.TextField(
+        editable=True, validators=[MaxLengthValidator(700)],
+        default='', blank=True
+    )
+    administrator_name = models.CharField(
+        max_length=200,
+    )
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        formatted_time = self.time_stamp.strftime("%Y-%m-%d %H:%M")
+        formatted_amount = "${:,}".format(self.transaction_amount)
+        return "2-to-1 Tutoring {}: {} with {} for {} at {}".format(
+            self.transaction_type.capitalize(),
+            self.primary_student_account.client_student_name,
+            self.shared_student_account.client_student_name,
+            formatted_amount,
+            formatted_time
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.transaction_type == 'payment':
+            self.primary_student_account.purchased_tutoring_hours += hours_as_decimal
+        else:
+            self.primary_student_account.purchased_tutoring_hours -= hours_as_decimal
+        self.student_account.save()
+
+    class Meta:
+        verbose_name_plural = '2-to-1 Tutoring Tuition Transaction Records'
+        ordering = ('-time_stamp',)
+
