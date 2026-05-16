f0bc1e06d5c03ba21d189e966990c26d66984300
phil-in-taipei <sweeney.philip11@gmail.com>
2026-05-16 15:13:35 +0800

    Wrote models for the group class, online class, and company class transactions, and modified the limit_choices_to null values restriction in the client school student accounts that can be selected for the respective transactions. I also wrote a line that would change null values to zero values for purchased class hour values prior to saving transactions and adding/subtracting hours -- django/backend

--- django/backend/client_school_transactions/models.py
+++ django/backend/client_school_transactions/models.py
@@ -32,7 +32,6 @@ class ClientSchoolTutoringTuitionTransactionRecord(models.Model):
     student_account = models.ForeignKey(
         AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
         related_name='tutoring_tuition_transactions',
-        limit_choices_to={'purchased_tutoring_hours__isnull': False}
     )
     transaction_amount = models.PositiveSmallIntegerField(
         validators=[validate_tuition_transaction_amount], default=33000
@@ -68,6 +67,8 @@ class ClientSchoolTutoringTuitionTransactionRecord(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.student_account.purchased_tutoring_hours is None:
+            self.student_account.purchased_tutoring_hours = Decimal('0')
         if self.transaction_type == 'payment':
             self.student_account.purchased_tutoring_hours += hours_as_decimal
         else:
@@ -83,12 +84,10 @@ class ClientSchool2to1TutoringTuitionTransactionRecord(models.Model):
     primary_student_account = models.ForeignKey(
         AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
         related_name='two_to_one_tutoring_tuition_transactions',
-        limit_choices_to={'purchased_tutoring_hours__isnull': False}
     )
     shared_student_account = models.ForeignKey(
         AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
         related_name='two_to_one_shared_tutoring_tuition_transactions',
-        limit_choices_to={'purchased_tutoring_hours__isnull': False}
     )
     transaction_amount = models.PositiveSmallIntegerField(
         validators=[validate_tuition_transaction_amount], default=16500
@@ -125,13 +124,174 @@ class ClientSchool2to1TutoringTuitionTransactionRecord(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.primary_student_account.purchased_tutoring_hours is None:
+            self.primary_student_account.purchased_tutoring_hours = Decimal('0')
         if self.transaction_type == 'payment':
             self.primary_student_account.purchased_tutoring_hours += hours_as_decimal
         else:
             self.primary_student_account.purchased_tutoring_hours -= hours_as_decimal
-        self.student_account.save()
+        self.primary_student_account.save()
 
     class Meta:
         verbose_name_plural = '2-to-1 Tutoring Tuition Transaction Records'
+        ordering = ('-time_stamp',)
+
+
+
+class ClientSchoolOnlineTuitionTransactionRecord(models.Model):
+    student_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
+        related_name='online_tuition_transactions',
+    )
+    transaction_amount = models.PositiveSmallIntegerField(
+        validators=[validate_tuition_transaction_amount],
+    )
+    transaction_type = models.CharField(
+        max_length=200, choices=TRANSACTION_TYPE, default='payment'
+    )
+    class_hours_purchased_or_refunded = models.PositiveSmallIntegerField(
+        validators=[validate_number_of_hours_purchased]
+    )
+    expiration_period = models.CharField(
+        max_length=200, choices=TUTORING_EXPIRATION_PERIODS, default='6_months'
+    )
+    comments = models.TextField(
+        editable=True, validators=[MaxLengthValidator(700)],
+        default='', blank=True
+    )
+    administrator_name = models.CharField(
+        max_length=200,
+    )
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        formatted_time = self.time_stamp.strftime("%Y-%m-%d %H:%M")
+        formatted_amount = "${:,}".format(self.transaction_amount)
+        return "Online Tutoring {}: {} for {} at {}".format(
+            self.transaction_type.capitalize(),
+            self.student_account.client_student_name,
+            formatted_amount,
+            formatted_time
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.student_account.purchased_online_hours is None:
+            self.student_account.purchased_online_hours = Decimal('0')
+        if self.transaction_type == 'payment':
+            self.student_account.purchased_online_hours += hours_as_decimal
+        else:
+            self.student_account.purchased_online_hours -= hours_as_decimal
+        self.student_account.save()
+
+    class Meta:
+        verbose_name_plural = 'Online Tuition Transaction Records'
+        ordering = ('-time_stamp',)
+
+
+
+class ClientSchoolGroupClassesTuitionTransactionRecord(models.Model):
+    student_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
+        related_name='group_class_tuition_transactions',
+    )
+    transaction_amount = models.PositiveSmallIntegerField(
+        validators=[validate_tuition_transaction_amount], default=33000
+    )
+    transaction_type = models.CharField(
+        max_length=200, choices=TRANSACTION_TYPE, default='payment'
+    )
+    class_hours_purchased_or_refunded = models.PositiveSmallIntegerField(
+        validators=[validate_number_of_hours_purchased]
+    )
+    expiration_period = models.CharField(
+        max_length=200, choices=GROUP_CLASS_EXPIRATION_PERIODS, default='12_weeks'
+    )
+    comments = models.TextField(
+        editable=True, validators=[MaxLengthValidator(700)],
+        default='', blank=True
+    )
+    administrator_name = models.CharField(
+        max_length=200,
+    )
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        formatted_time = self.time_stamp.strftime("%Y-%m-%d %H:%M")
+        formatted_amount = "${:,}".format(self.transaction_amount)
+        return "Group Classes {}: {} for {} at {}".format(
+            self.transaction_type.capitalize(),
+            self.student_account.client_student_name,
+            formatted_amount,
+            formatted_time
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.student_account.purchased_group_class_hours is None:
+            self.student_account.purchased_group_class_hours = Decimal('0')
+        if self.transaction_type == 'payment':
+            self.student_account.purchased_group_class_hours += hours_as_decimal
+        else:
+            self.student_account.purchased_group_class_hours -= hours_as_decimal
+        self.student_account.save()
+
+    class Meta:
+        verbose_name_plural = 'Group Classes Tuition Transaction Records'
+        ordering = ('-time_stamp',)
+
+
+
+class ClientSchoolCompanyClassesTuitionTransactionRecord(models.Model):
+    student_account = models.ForeignKey(
+        AccountingClientSchoolStudentAccount, on_delete=models.CASCADE,
+        related_name='company_class_tuition_transactions',
+    )
+    transaction_amount = models.PositiveSmallIntegerField(
+        validators=[validate_tuition_transaction_amount], default=33000
+    )
+    transaction_type = models.CharField(
+        max_length=200, choices=TRANSACTION_TYPE, default='payment'
+    )
+    class_hours_purchased_or_refunded = models.PositiveSmallIntegerField(
+        validators=[validate_number_of_hours_purchased]
+    )
+    expiration_period = models.CharField(
+        max_length=200, blank=True, default=''
+    )
+    comments = models.TextField(
+        editable=True, validators=[MaxLengthValidator(700)],
+        default='', blank=True
+    )
+    administrator_name = models.CharField(
+        max_length=200,
+    )
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        formatted_time = self.time_stamp.strftime("%Y-%m-%d %H:%M")
+        formatted_amount = "${:,}".format(self.transaction_amount)
+        return "Company Classes {}: {} for {} at {}".format(
+            self.transaction_type.capitalize(),
+            self.student_account.client_student_name,
+            formatted_amount,
+            formatted_time
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        hours_as_decimal = Decimal(str(self.class_hours_purchased_or_refunded))
+        if self.student_account.purchased_company_hours is None:
+            self.student_account.purchased_company_hours = Decimal('0')
+        if self.transaction_type == 'payment':
+            self.student_account.purchased_company_hours += hours_as_decimal
+        else:
+            self.student_account.purchased_company_hours -= hours_as_decimal
+        self.student_account.save()
+
+    class Meta:
+        verbose_name_plural = 'Company Classes Tuition Transaction Records'
         ordering = ('-time_stamp',)
 
