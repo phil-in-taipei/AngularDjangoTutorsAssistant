2be700a5a0abdcf504f8c3971118704a1a077ff1
phil-in-taipei <sweeneyphilip11@gmail.com>
2026-06-06 16:01:37 +0800

    Last modification to validation for transactions to allow for 0 value transactions for refunds and gift classes -- django/backend

--- django/backend/client_school_transactions/validation.py
+++ django/backend/client_school_transactions/validation.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_tuition_transaction_amount(value):
-    if value <= 175000:
+    if 0 <= value <= 175000:
         return value
     else:
         raise ValidationError("Tuition transactions for less than NT$175,000 only")
